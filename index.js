7e4bbfed9ed036bc3bbc4c267754102151bcc706
SOM-0x3B1 <somadagaszkar@gmail.com>
2022-04-22 18:55:10 +0200

    Implement event handling from discordjs.guide

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const { token } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
+
 client.commands = new Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     // Set a new item in the Collection
@@ -14,22 +14,16 @@ for (const file of commandFiles) {
     client.commands.set(command.data.name, command);
 }
 
-
-client.once('ready', () => {
-    console.log('Ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
-    const command = client.commands.get(interaction.commandName);
-    if (!command) return;
-
-    try { await command.execute(interaction); }
-    catch (error) {
-        console.error(error);
-        await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
     }
-});
+    else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
 
 
 client.login(token);
